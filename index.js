118e0bf324bb11930f4583374aece1450a6f61ae
hs02uly <hs02uly@gmail.com>
2022-08-23 19:50:11 +0900

    add: aタグの色々

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ let div = document.getElementsByClassName("member")
 const { Client } = require("discord.js")
 const client = new Client({ intents: 32767 })
 
-const serverIconURL = client.guilds.cache.get("941651004884152380").iconURL
+document.getElementsByID("serverIcon") = client.guilds.cache.get("941651004884152380").iconURL
 
 function getUserIconURL(ID){
     return client.users.fetch(ID).iconURL
     console.log(client.users.fetch(ID).iconURL)
+    console.log(client.guilds.cache.get("941651004884152380").iconURL)
 }
 
 $(function() {
