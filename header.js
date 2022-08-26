b6e4527970336814639c2ac8dfd942835f6c81ad
hs02uly <hs02uly@gmail.com>
2022-08-26 20:36:46 +0900

    update: header.js

--- header.js
+++ header.js
@@ -1,2 +1,11 @@
 const headerGen = document.getElementById("headerGen")
-headerGen.insertAdjacentHTML("afterbegin", '<nav><ul><li><a href="../../commander/">HOME</a></li><li><a href="../../commander/about/">ABOUT</a></li><li><a href="../../commander/history/">HISTORY</a></li><li><a href="../../commander/members/">MEMBERS</a></li></ul></nav>')
+const header = (function(param) {return param[0].replace(/\n|\r/g, "");})`
+<nav>
+    <ul>
+        <li><a href="../../commander/">HOME</a></li>
+        <li><a href="../../commander/about/">ABOUT</a></li>
+        <li><a href="../../commander/history/">HISTORY</a></li>
+        <li><a href="../../commander/members/">MEMBERS</a></li>
+    </ul>
+</nav>`
+headerGen.insertAdjacentHTML("afterbegin", header)
