216ce5fc79fb0d4563c0b027c09c5650afc6f9b0
hs02uly <hs02uly@gmail.com>
2022-08-26 21:34:59 +0900

    fix

--- header.js
+++ header.js
@@ -4,8 +4,9 @@ const header = (function(param) {return param[0].replace(/\n|\r/g, "");})`
     <ul>
         <li><a href="../../commander/">HOME</a></li>
         <li><a href="../../commander/about/">ABOUT</a></li>
-        <li><a href="../../commander/history/">HISTORY</a></li>
         <li><a href="../../commander/members/">MEMBERS</a></li>
+        <li><a href="../../commander/works/">WORKS</a></li>
+        <li><a href="../../commander/history/">HISTORY</a></li>
     </ul>
 </nav>`
 headerGen.insertAdjacentHTML("afterbegin", header)
