7c8519f493907af97b737a32913fb9df81ca0b9c
hs02uly <hs02uly@gmail.com>
2022-08-27 16:07:45 +0900

    update: この修正はいるのか…？？

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ const headerGen = document.getElementById("headerGen")
 const header = (function(param) {return param[0].replace(/\n|\r/g, "");})`
 <nav>
     <ul>
-        <li><a href="../../commander/">HOME</a></li>
+        <li><a href="../">HOME</a></li>
         <li><a href="../../commander/about/">ABOUT</a></li>
         <li><a href="../../commander/members/">MEMBERS</a></li>
         <li><a href="../../commander/works/">WORKS</a></li>
