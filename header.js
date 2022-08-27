13e701835218b20386c7c6d32d41b7cd4ea2cfe9
MIYUKINNGU <111403555+MIYUKINNGU@users.noreply.github.com>
2022-08-27 14:28:37 +0900

    ()←こいつつける呪いにかかったわ()

--- header.js
+++ header.js
@@ -2,11 +2,11 @@ const headerGen = document.getElementById("headerGen")
 const header = (function(param) {return param[0].replace(/\n|\r/g, "");})`
 <nav>
     <ul>
-        <li><a href="/">HOME</a></li>
-        <li><a href="/about/">ABOUT</a></li>
-        <li><a href="/history/">HISTORY</a></li>
-        <li><a href="/works/">WORKS</a></li>
-        <li><a href="/members/">MEMBERS</a></li>
+        <li><a href="/commander/">HOME</a></li>
+        <li><a href="/commander/about/">ABOUT</a></li>
+        <li><a href="/commander/history/">HISTORY</a></li>
+        <li><a href="/commander/works/">WORKS</a></li>
+        <li><a href="/commander/members/">MEMBERS</a></li>
     </ul>
 </nav>`
 headerGen.insertAdjacentHTML("afterbegin", header)
