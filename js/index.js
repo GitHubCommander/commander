95fd0a743daeb5594b0e8cf44742a778ebfca86a
hs02uly <hs02uly@gmail.com>
2022-08-29 08:40:24 +0900

    update: jsディレクトリ追加

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//以下鯖関連のアイコン等の画像取得↓
+const { Client } = require("discord.js")
+const client = new Client({ intents: 32767 })
+
+document.getElementsByID("serverIcon") = client.guilds.cache.get("941651004884152380").iconURL
+
+function getUserIconURL(ID){
+    return client.users.fetch(ID).iconURL
+    console.log(client.users.fetch(ID).iconURL)
+    console.log(client.guilds.cache.get("941651004884152380").iconURL)
+}
+
+$(function () {
+    setTimeout(function () {
+        $('.text_fadein').fadeIn(1000);
+    }, 500);
+});
